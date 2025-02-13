67f9d696db3eb5c1eca4848cd4c9d88b1774ae6b
stackheapderivative <awhardy42@tntech.edu>
2025-02-13 17:23:51 -0600

    Update main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+#This program is to provide a binary to hex, hex to binary and to decimal, etc. It will also allow for adding, subtracting and getting two's complement.
+
+def test():
+    print("This program is a test one!")
+
+test()
+
